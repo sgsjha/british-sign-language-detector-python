213128da4747fd0a9095216f7930b06ad4661c21
sgsjha <sarthakjhaa11@gmail.com>
2025-11-02 19:18:53 +0000

    Dataset creatio for A and B All steps completed

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,7 @@
 import os
 import mediapipe as mp
 import cv2
+import pickle
 import matplotlib.pyplot as plt
 
 
@@ -18,6 +19,9 @@ hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
 
+data = []
+labels = []
+
 for dir_ in os.listdir(DATA_DIR):
 
     # skip files like .DS_Store
@@ -25,7 +29,8 @@ for dir_ in os.listdir(DATA_DIR):
     if not os.path.isdir(dir_path):
         continue
 
-    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]:
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+        data_aux = []
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         # need to covert img to rgb to input into mediapipe
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -35,7 +40,7 @@ for dir_ in os.listdir(DATA_DIR):
         #iterate through results - show hands : DRAW LANDMARKS
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-
+                '''
                 #To show the drawing of landmarks : TESTING
                  mp.drawing.draw_landmarks(
                     img_rgb, #img to draw
@@ -44,12 +49,26 @@ for dir_ in os.listdir(DATA_DIR):
                     mp.drawing_styles.get_default_hand_landmarks_style(),
                     mp.drawing_styles.get_default_hand_connections_style()
                 )
+                '''
+
+                for i in range(len(hand_landmarks.landmark)):
+                    #create an array of landmarks with the x and y of the landmarks and train the mode
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
+                    data_aux.append(x)
+                    data_aux.append(y)
 
 
-        plt.figure()
-        plt.imshow(img_rgb)
+            data.append(data_aux)
+            labels.append(dir_)
 
-plt.show()
+f = open('data.pickle', 'wb')
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
+
+
+
+
 
 
 
