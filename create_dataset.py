0b3d688bf98ad1c5657c5e9a6ffda479814b8fac
sgsjha <sarthakjhaa11@gmail.com>
2025-11-02 18:55:00 +0000

    Hands landmark detection output"

--- create_dataset.py
+++ create_dataset.py
@@ -3,6 +3,15 @@ import mediapipe as mp
 import cv2
 import matplotlib.pyplot as plt
 
+
+mp_hands = mp.solutions.hands
+mp.drawing = mp.solutions.drawing_utils
+mp.drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+
+
 #iterate through the photos
 
 #save in a file to later train the model
@@ -20,6 +29,22 @@ for dir_ in os.listdir(DATA_DIR):
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
         # need to covert img to rgb to input into mediapipe
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        results = hands.process(img_rgb)
+
+        #iterate through results - show hands : DRAW LANDMARKS
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+
+                #To show the drawing of landmarks : TESTING
+                 mp.drawing.draw_landmarks(
+                    img_rgb, #img to draw
+                    hand_landmarks, #output
+                    mp_hands.HAND_CONNECTIONS, #hand connections
+                    mp.drawing_styles.get_default_hand_landmarks_style(),
+                    mp.drawing_styles.get_default_hand_connections_style()
+                )
+
 
         plt.figure()
         plt.imshow(img_rgb)
